5e92f42c932d51ec9e28b638dd9343db7c03a0b3
Laurent Planque <laurent.planque@intuiko.com>
2014-09-17 08:57:44 +0200

    Update some information

--- jsr353-morphia-converter/src/main/java/com/lplanque/math/package-info.java
+++ jsr353-morphia-converter/src/main/java/com/lplanque/math/package-info.java
@@ -1,6 +1,6 @@
 /**
  * This package contains the abstract type {@link com.lplanque.math.Bijection} that denotes a <I>bijection</I>
  * in the mathematical sense.
- * @author <a href="mailto:planquel@lild01.pictime.fr">planquel</a>
+ * @author <a href="https://github.com/lplanque" target="_blank">Laurent Planque</a>
  */
 package com.lplanque.math;
